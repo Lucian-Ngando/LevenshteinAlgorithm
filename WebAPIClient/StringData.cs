456c3a3fcd3da73aa973a491fd47dc5d8e16c6fc
Lucian Ngando <Lucian_live@outlook.dk>
2022-09-23 15:51:49 +0200

    Local testing accepoints

---
+++ WebAPIClient/StringData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPIClient
+{
+    public class StringData
+    {
+
+        private List<string> AllString = new List<string> {
+        "the Ioniq 5 electric-powered SUV, starring Holland and Batalon, with Watts directing. The Ioniq 5 and " +
+            "Hyundai Tucson are featured in the film. In late February 2021, Holland, " +
+            "Batalon, and Zendaya released three stills featuring their characters from the film " +
+            "alongside fake logos with the titles Spider-Man: Phone Home, Spider-Man: Home-Wrecker, and Spider-Man: Home Slice, respectively. " +
+            "The film's official title was announced the next day with a video showing " +
+            "Holland, Batalon, and Zendaya leaving Watts' office (where they supposedly received",
+        "in the media of discussions such as the negotiations do not necessarily line " +
+            "up with the actual discussions taking place, and he felt that the final deal would have eventuated without" +
+            " the reports and fan discourse, saying, We would have gotten there, and the news got ahead of some things." +
+            "Also in October, Zendaya was confirmed to be reprising her role of MJ from the previous films in the sequel. By the" +
+            " end of the year, filming was expected to",
+        "the fake titles). Batalon and Zendaya note that Holland could not be trusted with the actual title since he had " +
+            "accidentally" + "revealed the second film's title. The video ends on a whiteboard showing the film's" +
+            "true title, among various other titles featuring the word home that were apparently considered. Jennifer" +
+            "Bisset of CNET suggested the fake titles and logos could represent the villains in the film, including Foxx's" +
+            " Electro and Molina's Doctor Octopus, while TheWrap's Umberto Gonzalez called them funny",
+        "presale records of The Rise of Skywalker in Poland and No Time to Die in Portugal. " +
+            "Other markets with the best presale records include Spain, Brazil, and Central America.By August 2019, a fourth" +
+            " film in the franchise was in development alongside No Way Home. In February 2021, Holland said that while No Way" +
+            " Home was the final film under his contract with Marvel and Sony, he hoped to continue playing Spider-Man in the " +
+            "future if asked. That July, Zendaya said",
+        "unit filming occurred from October 14 to 16, 2020 in New York City, under the working title" +
+            " Serenity Now, to capture visual effects plates and establishing shots. Filming occurred in the Astoria, Sunnyside," +
+            " and Long Island City neighborhoods in Queens. On October 23, filming occurred in Greenwich Village in Manhattan." +
+            "The production moved to Atlanta by October 25, with Holland, Batalon, and Zendaya joining for principal" +
+            " photography, after Holland finished shooting Sony's Uncharted (2022) two days earlier. Mauro Fiore served as"
+
+        };
+
+        public List<string> AllStringData()
+        {
+            return AllString;
+        }
+
+    }
+}
